89127b584c177c2f19c3cf7bf64491959ca09113
Joel Mut <joelmut_2007@hotmail.com>
2016-11-26 20:04:52 -0300

    Push inicial

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require("gulp");
+var bs = require("browser-sync").create();
+var jade = require("gulp-jade");
+var sass = require("gulp-sass");
+var include = require("gulp-include");
+var autoprefixer = require("gulp-autoprefixer");
+var uglify = require("gulp-uglify");
+var pump = require('pump');
+
+// DIRECTORIOS
+var public = "./_public/";
+var images = "./images/";
+
+//TAREAS
+gulp.task('jade', function () {
+  pump([
+    gulp.src(['!./views/templates', 'views/*.jade']),
+    jade(),
+    gulp.dest(public)
+  ])
+});
+
+gulp.task('sass', function () {
+  pump([
+    gulp.src('styles/main.sass'),
+    sass({ outputStyle: 'compressed' }),
+    autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }),
+    gulp.dest(public),
+    bs.stream()
+  ])
+});
+
+gulp.task('javascript', function () {
+  pump([
+    gulp.src('scripts/main.js'),
+    include(),
+    uglify(),
+    gulp.dest(public),
+    bs.stream()
+  ])
+});
+
+gulp.task('copy-images', function () {
+  pump([
+    gulp.src(images),
+    gulp.dest(`${public}`)
+  ])
+});
+
+gulp.task('serve', function () {
+  bs.init({
+    server: public,
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0'
+      }
+    }
+  });
+
+  gulp.watch('styles/**/*.sass', ['sass']);
+  gulp.watch('views/**/*.jade', ['jade']);
+  gulp.watch('scripts/**/*.js', ['javascript']);
+
+});
+
+gulp.task("compile", ['jade', 'sass', 'javascript']);
+
+gulp.task("default", ['compile', 'copy-images', 'serve']);
---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+
+//Utils
+//=include utils/   
+
+//Libraries
+//=include libraries/jquery.js   
+
+//Base
+//=include base/  
+
+//Layuts
+//=include layouts/  
+
+//Components
+//=include components/form.js
+
+//Controllers
+//=include controllers/
+
+//Views
+//=include views/index.js
+
+//State
+//=include state/ 
+  
